9da365b092f8b5e92e5e5aa348b98e2e21b2045e
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-07-10 16:43:21 -0300

    exercicio_2

---
+++ DDL/exercicio_002.sql
@@ -0,0 +1,3 @@
+/*2. Altere a tabela "Clientes" para adicionar uma nova coluna chamada "Telefone" (texto, no máximo 15 caracteres).*/
+
+alter table clientes add s_telefone_clientes varchar(15) not null;
