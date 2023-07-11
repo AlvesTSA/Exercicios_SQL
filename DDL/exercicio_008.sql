fdc5afd69e5c8837f77252f5b5f43be223dc4e65
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-07-11 17:54:05 -0300

    exercicio_8

---
+++ DDL/exercicio_008.sql
@@ -0,0 +1,3 @@
+/* 8. Altere a tabela "Produtos" para modificar o nome da coluna "Preco" para "PrecoUnitario".*/
+
+alter table produtos rename column de_preco_produtos to de_precoUnitario_produtos;
